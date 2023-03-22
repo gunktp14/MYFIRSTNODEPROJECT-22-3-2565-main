aff304b3664e76809870f668a8ca35a0fe0870d4
gunktp14 <120033691+gunktp14@users.noreply.github.com>
2023-03-22 19:17:00 +0700

    Initial commit

---
+++ calculator.test.js
@@ -0,0 +1,39 @@
+const mathOperations = require('./calculator');
+//
+describe("Calculator tests", () => {
+
+    beforeAll(() => {
+        console.log("beforeAll called");
+    });
+
+    afterAll(() => {
+        console.log("afterAll called");
+    });
+
+    beforeEach(() => {
+        console.log("beforeEach called");
+    });
+    
+    afterEach(() => {
+        console.log("afterEach called");
+    });
+
+    //
+    test('adding 1 + 2 should return 3', () => {
+        var result = mathOperations.sum(1, 2);
+        var string1 = "software testing help - a great resource for testers";
+
+        expect(result).toBe(3);
+        expect(result).not.toBe(5);
+        expect(string1).not.toMatch(/abc/);
+    });
+
+    test('adding 5 - 1 should return 4', () => {
+        var result = mathOperations.diff(5, 1);
+        expect(result).toBe(4);
+    });
+    test('adding 7 * 3 should return 21', () => { //การคำนวณ
+        var result = mathOperations.product(7, 3); //ตัวเลขที่คำนวณ
+        expect(result).toBe(21); //ผลลัพธ์
+    });
+})
